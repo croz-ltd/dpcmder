b2d84d491e0538e41403ae5233881b6703700fbc
Vedran Vidović <vvidovic@gmail.com>
2019-10-25 08:24:26 +0200

    Handling Ctrl+C event$
    On some consoles termination of program caused console to become garbled so$
    closing of termbox on ctrl+c is added to avoid this.

--- dpcmder.go
+++ dpcmder.go
@@ -6,6 +6,10 @@ import (
 	"github.com/croz-ltd/dpcmder/repo/dp"
 	"github.com/croz-ltd/dpcmder/utils/logging"
 	"github.com/croz-ltd/dpcmder/view"
+	"github.com/nsf/termbox-go"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -17,7 +21,26 @@ func main() {
 		model.M.SetDpAppliance(config.PreviousAppliance)
 	}
 
+	setupCloseHandler()
+
 	view.Init()
 	// model.M.Print()
 	logging.LogDebug("...dpcmder ending.")
+}
+
+// SetupCloseHandler creates a 'listener' on a new goroutine which will notify the
+// program if it receives an interrupt from the OS - so we can cleanup/close termbox.
+func setupCloseHandler() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, os.Interrupt, syscall.SIGKILL)
+	signal.Notify(c, os.Interrupt, syscall.SIGINT)
+	signal.Notify(c, os.Interrupt, syscall.SIGHUP)
+	signal.Notify(c)
+	go func() {
+		s := <-c
+		logging.LogDebug("System interrupt signal received, dpcmder ending. s: ", s)
+		termbox.Close()
+		os.Exit(0)
+	}()
 }
