c12395f3d42fe62cbd508b5b1992e182a16c59de
Vedran Vidović <vvidovic@gmail.com>
2019-11-27 11:54:06 +0100

    Renamed main input loop
    We catch all input events (key press and resize are used).

--- ui/input.go
+++ ui/input.go
@@ -14,26 +14,25 @@ import (
 func StartReadingKeys() {
 	logging.LogDebug("ui/in/Start()")
 
-	keyPressedLoop()
+	inputEventLoop()
 }
 
-// keyPressedLoop is main loop reading user's input.
-func keyPressedLoop() {
-	logging.LogDebug("ui/keyPressedLoop() starting")
+// inputEventLoop is main loop reading user's input.
+func inputEventLoop() {
+	logging.LogDebug("ui/inputEventLoop() starting")
 
 loop:
 	for {
-		logging.LogTrace("ui/keyPressedLoop(), waiting to read")
+		logging.LogTrace("ui/inputEventLoop(), waiting to read")
 		event := out.Screen.PollEvent()
-		// keyCode, err := kcr.readNext()
 
-		logging.LogTracef("ui/keyPressedLoop(), event: %#v", event)
+		logging.LogTracef("ui/inputEventLoop(), event: %#v", event)
 		err := ProcessInputEvent(event)
 		if err != nil {
 			break loop
 		}
 	}
-	logging.LogDebug("ui/keyPressedLoop() stopping")
+	logging.LogDebug("ui/inputEventLoop() stopping")
 }
 
 ////
